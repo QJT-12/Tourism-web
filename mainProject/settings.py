d2dedf86ac0159c7a019483095195239d1efb1db
QJT-12 <qwe.lawi@gmail.com>
2025-03-11 18:21:46 +0300

    Update settings.py

--- mainProject/settings.py
+++ mainProject/settings.py
@@ -126,3 +126,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGIN=["https://tourism-web-production.up.railway.app"]
